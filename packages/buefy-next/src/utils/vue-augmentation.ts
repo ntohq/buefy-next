2bdcf87d8cef2a2df8f39dde5402104064869883
Kikuo Emoto <kemoto@codemonger.io>
2024-03-29 15:08:54 +0900

    feat(lib): add snackbar to $buefy
    Binds `$buefy.snackbar` to `SnackbarProgrammatic`

--- packages/buefy-next/src/utils/vue-augmentation.ts
+++ packages/buefy-next/src/utils/vue-augmentation.ts
@@ -7,6 +7,7 @@
 // otherwise the post processing will be messed up.
 import 'vue'
 
+import type { SnackbarProgrammatic } from '../components/snackbar'
 import ConfigComponent from './ConfigComponent'
 
 // Augments the global property with `$buefy`.
@@ -18,6 +19,7 @@ declare module '@vue/runtime-core' {
         $buefy: {
             config: typeof ConfigComponent,
             globalNoticeInterval?: ReturnType<typeof setTimeout>,
+            snackbar: SnackbarProgrammatic,
             // TODO: make key-values more specific
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             [key: string]: any
