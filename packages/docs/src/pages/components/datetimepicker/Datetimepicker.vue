f533073f23f2aa6e2ea8d2090661187274bbaf13
Kikuo Emoto <kemoto@codemonger.io>
2024-10-20 13:31:45 +0900

    SQUASH(docs): import docs components in datetimepicker example
    `src/pages/components/datetimepicker/Datetimepicker.vue` explicitly
    imports and registers the following components:
    - `ApiView`
    - `Example`

--- packages/docs/src/pages/components/datetimepicker/Datetimepicker.vue
+++ packages/docs/src/pages/components/datetimepicker/Datetimepicker.vue
@@ -29,8 +29,11 @@
 <script lang="ts">
     import { defineComponent } from 'vue'
 
-    import api from './api/datetimepicker'
     import { shallowFields } from '@/utils'
+    import ApiView from '@/components/ApiView.vue'
+    import Example from '@/components/Example.vue'
+
+    import api from './api/datetimepicker'
 
     import ExSimple from './examples/ExSimple.vue'
     import ExSimpleCode from './examples/ExSimple.vue?raw'
@@ -51,6 +54,10 @@
     import ExGranularityCode from './examples/ExGranularity.vue?raw'
 
     export default defineComponent({
+        components: {
+            ApiView,
+            Example
+        },
         data() {
             return {
                 api,
