11f8bada9b7cd6cec484d4752c852041a4e9bf37
Kikuo Emoto <kemoto@codemonger.io>
2024-10-20 16:28:18 +0900

    SQUASH(docs): import docs components in switch example
    `src/pages/components/switch/Switch.vue` explicitly imports and
    registers the following components:
    - `ApiView`
    - `Example`
    - `VariablesView`

--- packages/docs/src/pages/components/switch/Switch.vue
+++ packages/docs/src/pages/components/switch/Switch.vue
@@ -16,9 +16,13 @@
 <script lang="ts">
     import { defineComponent } from 'vue'
 
+    import { shallowFields } from '@/utils'
+    import ApiView from '@/components/ApiView.vue'
+    import Example from '@/components/Example.vue'
+    import VariablesView from '@/components/VariablesView.vue'
+
     import api from './api/switch'
     import variables from './variables/switch'
-    import { shallowFields } from '@/utils'
 
     import ExSimple from './examples/ExSimple.vue'
     import ExSimpleCode from './examples/ExSimple.vue?raw'
@@ -33,6 +37,11 @@
     import ExStylesCode from './examples/ExStyles.vue?raw'
 
     export default defineComponent({
+        components: {
+            ApiView,
+            Example,
+            VariablesView
+        },
         data() {
             return {
                 api,
