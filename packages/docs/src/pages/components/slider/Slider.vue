b42e228a3bf9c93eb9c51529e246b9f713358c2e
Kikuo Emoto <kemoto@codemonger.io>
2024-10-20 16:22:55 +0900

    SQUASH(docs): import docs components in slider example
    `src/pages/components/slider/Slider.vue` explicitly imports and
    registers the following components:
    - `ApiView`
    - `Example`
    - `VariablesView`

--- packages/docs/src/pages/components/slider/Slider.vue
+++ packages/docs/src/pages/components/slider/Slider.vue
@@ -33,9 +33,13 @@
 <script lang="ts">
     import { defineComponent } from 'vue'
 
+    import { shallowFields } from '@/utils'
+    import ApiView from '@/components/ApiView.vue'
+    import Example from '@/components/Example.vue'
+    import VariablesView from '@/components/VariablesView.vue'
+
     import api from './api/slider'
     import variables from './variables/slider'
-    import { shallowFields } from '@/utils'
 
     import ExSimple from './examples/ExSimple.vue'
     import ExSimpleCode from './examples/ExSimple.vue?raw'
@@ -55,6 +59,11 @@
     import ExIndicatorCode from './examples/ExIndicator.vue?raw'
 
     export default defineComponent({
+        components: {
+            ApiView,
+            Example,
+            VariablesView
+        },
         data() {
             return {
                 api,
