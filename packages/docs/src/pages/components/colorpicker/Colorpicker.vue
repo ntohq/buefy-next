a8efaaabbcfe5beb1b0b15048e7087ea99fbb920
Kikuo Emoto <kemoto@codemonger.io>
2024-10-20 13:27:28 +0900

    SQUASH(docs): import docs components in colorpicker example
    `src/pages/components/colorpicker/Colorpicker.vue` explicitly imports
    and registers the following components:
    - `ApiView`
    - `Example`
    - `VariablesView`

--- packages/docs/src/pages/components/colorpicker/Colorpicker.vue
+++ packages/docs/src/pages/components/colorpicker/Colorpicker.vue
@@ -18,9 +18,13 @@
 <script lang="ts">
     import { defineComponent } from 'vue'
 
+    import { shallowFields } from '@/utils'
+    import ApiView from '@/components/ApiView.vue'
+    import Example from '@/components/Example.vue'
+    import VariablesView from '@/components/VariablesView.vue'
+
     import api from './api/colorpicker'
     import variables from './variables/colorpicker'
-    import { shallowFields } from '@/utils'
 
     import ExSimple from './examples/ExSimple.vue'
     import ExSimpleCode from './examples/ExSimple.vue?raw'
@@ -38,6 +42,11 @@
     import ExFieldsCode from './examples/ExFields.vue?raw'
 
     export default defineComponent({
+        components: {
+            ApiView,
+            Example,
+            VariablesView
+        },
         data() {
             return {
                 api,
