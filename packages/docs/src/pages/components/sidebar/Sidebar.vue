95dad1bdd6030a8666b12e56b2a24bbc17fe9bf8
Kikuo Emoto <kemoto@codemonger.io>
2024-10-20 16:18:47 +0900

    SQUASH(docs): import docs components in sidebar example
    `src/pages/components/sidebar/Sidebar.vue` explicitly imports and
    registers the following components:
    - `ApiView`
    - `Example`
    - `VariablesView`

--- packages/docs/src/pages/components/sidebar/Sidebar.vue
+++ packages/docs/src/pages/components/sidebar/Sidebar.vue
@@ -13,9 +13,13 @@
 <script lang="ts">
     import { defineComponent } from 'vue'
 
+    import { shallowFields } from '@/utils'
+    import ApiView from '@/components/ApiView.vue'
+    import Example from '@/components/Example.vue'
+    import VariablesView from '@/components/VariablesView.vue'
+
     import api from './api/sidebar'
     import variables from './variables/sidebar'
-    import { shallowFields } from '@/utils'
 
     import ExSimple from './examples/ExSimple.vue'
     import ExSimpleCode from './examples/ExSimple.vue?raw'
@@ -23,6 +27,11 @@
     import ExStaticCode from './examples/ExStatic.vue?raw'
 
     export default defineComponent({
+        components: {
+            ApiView,
+            Example,
+            VariablesView
+        },
         data() {
             return {
                 api,
