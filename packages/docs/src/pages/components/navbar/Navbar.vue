40c0393a8882ad9f1b340de2a534c10b83cc4483
Kikuo Emoto <kemoto@codemonger.io>
2024-10-20 15:18:00 +0900

    SQUASH(docs): import docs components in navbar example
    `src/pages/components/navbar/Navbar.vue` explicitly imports and
    registers the following components:
    - `ApiView`
    - `Example`
    - `VariablesView`

--- packages/docs/src/pages/components/navbar/Navbar.vue
+++ packages/docs/src/pages/components/navbar/Navbar.vue
@@ -11,14 +11,23 @@
 <script lang="ts">
     import { defineComponent } from 'vue'
 
+    import { shallowFields } from '@/utils'
+    import ApiView from '@/components/ApiView.vue'
+    import Example from '@/components/Example.vue'
+    import VariablesView from '@/components/VariablesView.vue'
+
     import api from './api/navbar'
     import variables from './variables/navbar'
-    import { shallowFields } from '@/utils'
 
     import ExSimple from './examples/ExSimple.vue'
     import ExSimpleCode from './examples/ExSimple.vue?raw'
 
     export default defineComponent({
+        components: {
+            ApiView,
+            Example,
+            VariablesView
+        },
         data() {
             return {
                 api,
