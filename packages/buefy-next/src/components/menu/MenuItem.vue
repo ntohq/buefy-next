e12f0bb68bc68c620d44a9165ad32758f4b7f2ce
Sebastian Landwehr <info@sebastianlandwehr.com>
2024-12-17 01:23:54 +0000

    foo

--- packages/buefy-next/src/components/menu/MenuItem.vue
+++ packages/buefy-next/src/components/menu/MenuItem.vue
@@ -70,7 +70,7 @@ export default {
             type: [String, Object],
             default: 'a',
             validator: (value) => {
-                return config.defaultLinkTags.indexOf(value) >= 0
+                return typeof value === 'object' || config.defaultLinkTags.indexOf(value) >= 0
             }
         },
         ariaRole: {
