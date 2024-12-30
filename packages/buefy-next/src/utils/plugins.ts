64804481ffbf0b6745af1c8f32cbe27ff2a1063c
Kikuo Emoto <kemoto@codemonger.io>
2024-12-30 23:55:18 +0900

    chore(lib): remove global Vue and use function
    The global Vue object, and the `use` function are no longer necessary.
    They are legacies of Vue 2.

--- packages/buefy-next/src/utils/plugins.ts
+++ packages/buefy-next/src/utils/plugins.ts
@@ -1,20 +1,4 @@
-import type { App, Component, Plugin } from 'vue'
-
-// TODO: `use` shall be removed and this declaration shall also be removed.
-declare global {
-    interface Window {
-        Vue?: App
-    }
-}
-
-// same signature as `App.use`: https://github.com/vuejs/core/blob/ee4cd78a06e6aa92b12564e527d131d1064c2cd0/packages/runtime-core/src/apiCreateApp.ts#L36-L39
-// TODO: `use` no longer makes sense in Vue 3.
-export const use: <T extends unknown[]>(plugin: Plugin<T>, ...options: T) => void =
-    (plugin, ...options) => {
-        if (typeof window !== 'undefined' && window.Vue) {
-            window.Vue.use(plugin, options)
-        }
-    }
+import type { App, Component } from 'vue'
 
 // use `name` to register a Functional Component which will become unresolvable
 // in production build due to name mangling.
